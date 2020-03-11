90a2018f5770603d09b13d70ad738ba59a6285dc
jimbolit <james.little@kellogg.northwestern.edu>
2020-03-11 15:57:32 +0000

    Setup transfer from jimmylit

--- app.rb
+++ app.rb
@@ -14,5 +14,31 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+purchases_table = DB.from(:purchases)
+bandwagoners_table = DB.from(:bandwagoners)
+
+get "/" do
+    @purchases = purchases_table.all.to_a
+    view "purchases"
+end
+
+get "/purchase/:id" do
+    @purchase = purchases_table.where(id: params["id"]).to_a[0]
+    view "purchase"
+end
+
+get "/purchase/:id/bandwagoner/new" do
+    @purchase = purchases_table.where(id: params[:id]).to_a[0]
+    view "new_bandwagoner"
+end
+
+get "/purchase/:id/bandwagon/thanks" do
+    puts params
+    @purchase = purchases_table.where(id: params[:id]).to_a[0]
+    puts @purchase
+    bandwagoners_table.insert(purchase_id: params["id"],
+                       user_id: 1,
+                       onwagon: params["onwagon"],
+                       comments: params["comments"])
+    view "bandwagon_thanks"
+end
